65b717bee9b1e9d0125f717c447274c33284e68d
Damiano Massarelli <damiano.massarelli@gmail.com>
2019-01-26 11:13:40 +0100

    switch to c++11 random number generation

--- src/utils/randomUtils.cpp
+++ src/utils/randomUtils.cpp
@@ -1,12 +1,16 @@
 #include "randomUtils.h"
-#include <cstdlib>
+#include <random>
+
+std::random_device rd;
+std::mt19937 mt(rd());
 
 float randRange(float start, float end) {
-    float f = static_cast<float>(std::rand())/(RAND_MAX + 1u);
-    return start + (end - start)*f;
+    std::uniform_real_distribution<float> dist(start, end);
+    return dist(mt);
 }
 
 int randRangeInt(int start, int end)
 {
-    return static_cast<int>(randRange(start, end));
+    std::uniform_int_distribution<int> dist(start, end - 1);
+    return dist(mt);
 }
